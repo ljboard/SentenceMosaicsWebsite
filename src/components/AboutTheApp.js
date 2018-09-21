9a94742188937ea5fde665461a56e47fc7368e0c
ljboard <ljboard@gmail.com>
2018-09-21 16:05:14 -0400

    Grammar fixes

--- src/components/AboutTheApp.js
+++ src/components/AboutTheApp.js
@@ -8,7 +8,7 @@ class AboutTheApp extends Component {
       <div>
         <h1>The Project</h1>
         <h3>Overview</h3>
-        <p>We are excited to share a new, free, speech-language therapy app developed through the mutual efforts of Carnegie Mellon University students on the Design for America Team and a Pittsburgh-based speech-language pathologist, Alyse O. Baker, CCC-SLP, and was launched in 2018. </p>
+        <p>We are excited to share a new, free, speech-language therapy app developed through the mutual efforts of Carnegie Mellon University students on the Design for America Team and a Pittsburgh-based speech-language pathologist, Alyse O. Baker, CCC-SLP.  The app was launched in April 2018. </p>
         <h3>Purpose</h3>
         <p>The purpose of this app is to assist students in their formulation of novel, complete, and meaningful spoken sentences produced in context with a photograph or illustration and with a language “coach” (speech-language therapist, teacher, parent, tutor, etc.) to assist in facilitating correct syntax. </p>
         <h3>Users</h3>
@@ -33,7 +33,7 @@ class AboutTheApp extends Component {
         </Col>
       </Row>
       <br/>
-      <p>These types of tools are not the most efficient because of several reasons. First, the foam board is too bulky and not easily transportable, which makes it inconvenient for students and parents who want to utilize the tool outside of class time. Second, even though the velcro used to change the order of the parts of speech allows students to create a variety of different sentences, the vocabulary blocks themselves are not customizable enough and don’t have a wide range of options. Because of these limiting aspects of the foam board, it was decided that an app would greatly improve this system, and greatly benefit the students’ learning.</p>
+      <p>These types of tools are not the most efficient for several reasons. First, the foam board is too bulky and not easily transportable, which makes it inconvenient for students and parents who want to utilize the tool outside of class time. Second, even though the velcro used to change the order of the parts of speech allows students to create a variety of different sentences, the vocabulary blocks themselves are not customizable enough and don’t have a wide range of options. Because of these limiting aspects of the foam board, it was decided that an app would greatly improve this system, and greatly benefit the students’ learning.</p>
       <h3>Research in Speech Pathology</h3>
       <p>This app builds upon the original work of Edith Fitzgerald who published Straight Language for the Deaf in 1926. The Fitzgerald Key uses unique colors to represent different parts of speech (noun as orange, pronoun as yellow, verb as green, etc.) as a tool for instruction. In this app, additional categories for prepositions, punctuation, and functional usage of words (“social” and “wh-words”) were added to diversify the vocabulary and allow students to construct more complex sentences.</p>
       </div>
